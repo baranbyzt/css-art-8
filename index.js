c271c113d866928084c4ac05e7094c135b453937
baran beyazıt <byztmain2121@gmail.com>
2022-01-17 15:57:26 +0300

    frontend is finished

---
+++ index.js
@@ -0,0 +1,70 @@
+
+let item_q = $(".item_q");
+let item_w = $(".item_w");
+let item_e = $(".item_e");
+let item_r = $(".item_r");
+let item_t = $(".item_t");
+let item_y = $(".item_y");
+let item_u = $(".item_u");
+let item_ı = $(".item_ı");
+let item_o = $(".item_o");
+let item_p = $(".item_p");
+let item_ğ = $(".item_ğ");
+let item_ü = $(".item_ü");
+let item_a = $(".item_a");
+let item_s = $(".item_s");
+let item_d = $(".item_d");
+let item_f = $(".item_f");
+let item_g = $(".item_g");
+let item_h = $(".item_h");
+let item_j = $(".item_j");
+let item_k = $(".item_k");
+let item_l = $(".item_l");
+let item_ş = $(".item_ş");
+let item_i = $(".item_i");
+let item_z = $(".item_z");
+let item_x = $(".item_x");
+let item_c = $(".item_c");
+let item_v = $(".item_v");
+let item_b = $(".item_b");
+let item_n = $(".item_n");
+let item_m = $(".item_m");
+let item_ö = $(".item_ö");
+let item_ç = $(".item_ç");
+
+
+
+$(document).ready(function(){
+
+    item_ç.click(function(){
+alert("ticket ç "+item_ç)
+    })
+    
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
